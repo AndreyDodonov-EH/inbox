6ede9f3a797523128bedfe8ae9af41750cbe150e
Andrey Dodonov <Andrey.Dodonov@endress.com>
2024-10-18 10:55:35 +0200

    Just formatting

--- tests/Inbox.test.js
+++ tests/Inbox.test.js
@@ -1,13 +1,13 @@
 const assert = require('assert'); // assertion library
 const ganache = require('ganache'); // local test network
-const {Web3} = require('web3');
+const { Web3 } = require('web3');
 const compile = require('../compile');
 
 const ganacheOptions = {
-    chain: {hardfork: 'shanghai'},
-    logging: {debug: true}
-  };
- 
+    chain: { hardfork: 'shanghai' },
+    logging: { debug: true }
+};
+
 const provider = ganache.provider(ganacheOptions);
 const web3 = new Web3(provider);
 
@@ -20,35 +20,36 @@ beforeEach(async () => {
     const web3Contract = new web3.eth.Contract(contract.abi);
     // THIS IS NOT DEPLOYING THE CONTRACT, IT IS CREATING DEPLOYMENT TRANSACTION WHICH SHOULD BE SENT
     const deploymentTranscation = web3Contract
-         .deploy({data: contract.evm.bytecode.object, arguments: [initialMessage]});
-    
+        .deploy({ data: contract.evm.bytecode.object, arguments: [initialMessage] });
+
     accounts = await web3.eth.getAccounts();
-    inbox = await deploymentTranscation.send({from: accounts[0],  gas: '1000000',
+    inbox = await deploymentTranscation.send({
+        from: accounts[0], gas: '1000000',
         maxFeePerGas: web3.utils.toWei('5', 'gwei'),
-        maxPriorityFeePerGas: web3.utils.toWei('3.5', 'gwei') 
-        }).on('receipt', function(receipt) {
-            console.log('Contract deployed at address:', receipt.contractAddress);
-        })
-        .on('error', function(error) {
+        maxPriorityFeePerGas: web3.utils.toWei('3.5', 'gwei')
+    }).on('receipt', function (receipt) {
+        console.log('Contract deployed at address:', receipt.contractAddress);
+    })
+        .on('error', function (error) {
             console.error('Error deploying contract:', error);
         });
 });
- 
+
 describe('Inbox', () => {
     it('can deploy', () => {
-        assert.ok(inbox.options.address); 
+        assert.ok(inbox.options.address);
         console.log('METHODS START');
         console.log(inbox.methods);
         console.log('METHODS END');
     });
-    it ('has a default message', async () => {
+    it('has a default message', async () => {
         const message = await inbox.methods.message().call();
         assert.strictEqual(message, initialMessage);
     });
 
-    it ('can change the message', async () => {
+    it('can change the message', async () => {
         const newMessage = 'Temporary gays!';
-        await inbox.methods.setMessage(newMessage).send( {from: accounts[0]});
+        await inbox.methods.setMessage(newMessage).send({ from: accounts[0] });
         const message = await inbox.methods.message().call();
         assert.strictEqual(message, newMessage);
     });
